a5143b8e15126c07d1227e087abfa3b28cefb49e
AmineChakr <chakrelllah@gmail.com>
2020-06-28 17:06:25 +0200

    modify instance arguments

--- EKS terraform/eks-worker-nodes.tf
+++ EKS terraform/eks-worker-nodes.tf
@@ -43,7 +43,7 @@ resource "aws_eks_node_group" "demo" {
   node_group_name = "demo"
   node_role_arn   = aws_iam_role.demo-node.arn
   subnet_ids      = aws_subnet.demo[*].id
-  instance_types  = "t2.small"
+  instance_types  = ["t2.small"]
 
   scaling_config {
 

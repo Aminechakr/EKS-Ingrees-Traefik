670f88b288401df66fadaf29c9c92480f938f89a
AmineChakr <chakrelllah@gmail.com>
2020-07-01 02:35:20 +0200

    fix

--- EKS Terraform with exsisting vpc/eks-cluster.tf
+++ EKS Terraform with exsisting vpc/eks-cluster.tf
@@ -89,7 +89,7 @@ resource "aws_eks_cluster" "demo" {
   vpc_config {
     security_group_ids = [aws_security_group.demo-cluster.id]
     subnet_ids         = [
-      var.subnet_a
+      var.subnet_a,
       var.subnet_b
     ]
      # = aws_subnet.demo[*].id
